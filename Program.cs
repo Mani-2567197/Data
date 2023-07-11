b004ec66df5b54b1717dd8b9b55023938d5135a1
unknown <manikanta.official1408@gmail.com>
2023-07-11 12:04:51 +0530

    da

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string username;
+        Again:
+            Console.WriteLine("Enter the userName");
+            username = Console.ReadLine();
+            if (username.Length >= 6)
+            {
+                Console.WriteLine("Welcome : \t " + username);
+            }
+            else
+            {
+                Console.WriteLine("Invalid User Name");
+                goto Again;
+            }
+            Console.ReadKey();
+        }
+    }
+}
